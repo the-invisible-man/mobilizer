c9d68d641d3a6264371c335f74f7a65de4a2096c
root <root@homestead>
2016-02-19 19:14:05 +0000

    Initial commit

---
+++ app/Lib/Packages/Feed/Controllers/FeedController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Lib\Packages\Feed\Controllers;
+
+use App\Http\Controllers\Controller;
+
+/**
+ * Class FeedController
+ *
+ * @author Carlos Granados <granados.carlos91@gmail.com>
+ * @package Lib\Packages\Feed\Controllers
+ */
+class FeedController extends Controller {
+
+}
---
+++ app/Lib/Packages/Post/Models/Post.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Lib\Packages\Post\Models;
+
+/**
+ * Class Post
+ *
+ * @author Carlos Granados <granados.carlos91@gmail.com>
+ *
+ * @package Core
+ */
+class Post {
+
+
+    public function __construct() {
+
+    }
+}

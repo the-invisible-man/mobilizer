98c4b532aaafc8f1c370891252780aeee302b6b2
The Invisible Man <granados.carlos91@gmail.com>
2016-06-23 22:59:13 -0400

    Updated icons

--- resources/views/layouts/user_side_bar.blade.php
+++ resources/views/layouts/user_side_bar.blade.php
@@ -4,13 +4,15 @@
         <h5>{{$auth['userInfo']['first_name']}} {{$auth['userInfo']['last_name']}}</h5>
     </div>
     <ul class="list user-profile-nav">
-        <li><a href="/my-listings"><i class="fa fa-camera"></i>My Listings</a>
+        <li><a href="/my-listings"><i class="fa fa-home"></i>Home</a>
         </li>
-        <li><a href="/my-requests"><i class="fa fa-camera"></i>Requests</a>
+        <li><a href="/my-listings"><i class="fa fa-calendar"></i>My Listings</a>
         </li>
-        <li><a href="/bookings"><i class="fa fa-clock-o"></i>My Bookings</a>
+        <li><a href="/my-requests"><i class="fa fa-envelope"></i>Requests</a>
         </li>
-        <li><a href="/logout"><i class="fa fa-clock-o"></i>Sign Out</a>
+        <li><a href="/bookings"><i class="fa fa-book"></i>My Bookings</a>
+        </li>
+        <li><a href="/logout"><i class="fa fa-lock"></i>Sign Out</a>
         </li>
     </ul>
 </aside>

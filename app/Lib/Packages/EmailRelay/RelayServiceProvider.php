d0e93bc3837e8ac51750434ed22651bdef5cb869
The Invisible Man <granados.carlos91@gmail.com>
2016-06-23 21:51:12 -0400

    Fixed service provider"

--- app/Lib/Packages/EmailRelay/RelayServiceProvider.php
+++ app/Lib/Packages/EmailRelay/RelayServiceProvider.php
@@ -6,13 +6,18 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Mail\Mailer;
 use Illuminate\Database\DatabaseManager;
+use App\Lib\Packages\EmailRelay\RelayGateway;
 
 class RelayServiceProvider extends ServiceProvider
 {
     public function register()
     {
         $this->app->singleton(Postmaster::class, function (Application $app) {
-            return new Postmaster($app['config']['mail.relay'], $app->make(Mailer::class), $app->make(DatabaseManager::class));
+            return new Postmaster($app['config']['mail.relay'], $app->make(Mailer::class), $app->make(DatabaseManager::class), $app->make(RelayGateway::class));
+        });
+
+        $this->app->singleton(RelayGateway::class, function (Application $app) {
+            return new RelayGateway($app->make('db'));
         });
     }
 }

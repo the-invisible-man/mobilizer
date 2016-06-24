e5a19138448d58231fabb15e3422ee4b0681474e
The Invisible Man <granados.carlos91@gmail.com>
2016-06-23 21:06:09 -0400

    Authorization

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -72,7 +72,7 @@ class AuthController extends Controller
             'last_name'     => 'required|max:255',
             'email'         => 'required|email|max:255|unique:users',
             'password'      => 'required|confirmed|min:6',
-            'g-recaptcha-response' => 'required|recaptcha'
+            // 'g-recaptcha-response' => 'required|recaptcha'
         ]);
     }
 
--- resources/views/auth/signup_confirm.blade.php
+++ resources/views/auth/signup_confirm.blade.php
@@ -3,7 +3,7 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12" style="padding-top: 100px;">
-                Hey thanks for signing up! We sent a confirmation to email to {{$email}}. If it's not there yet give it a couple of minutes or check your spam box.
+                Hey thanks for signing up! We sent a confirmation to {{$email}}. If it's not there yet give it a couple of minutes or check your spam box.
             </div>
         </div>
     </div>

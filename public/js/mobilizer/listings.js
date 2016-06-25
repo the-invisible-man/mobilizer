1d29a47e674e856a56923d94a17dcc1db6d7055b
The Invisible Man <granados.carlos91@gmail.com>
2016-06-25 01:08:38 -0400

    changed stroke color, fixed background scroll on modal open

--- public/js/mobilizer/listings.js
+++ public/js/mobilizer/listings.js
@@ -111,10 +111,10 @@
 
         var map             = MyListings.EditWindow.map;
         var tripRoute       = new google.maps.Polyline({
-            strokeColor: '#FF0000',
-            strokeOpacity: 0.8,
-            strokeWeight: 2,
-            fillColor: '#FF0000',
+            strokeColor: '#0066cc',
+            strokeOpacity: 1,
+            strokeWeight: 4,
+            fillColor: '#3399ff',
             map: map
         });
 
